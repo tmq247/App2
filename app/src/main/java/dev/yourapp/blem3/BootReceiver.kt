c8206212af481fea80c9076aaeae6df5e6e770cf
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-11-03 02:06:54 +0700

    Update BootReceiver.kt

--- app/src/main/java/dev/yourapp/blem3/BootReceiver.kt
+++ app/src/main/java/dev/yourapp/blem3/BootReceiver.kt
@@ -1,10 +1,13 @@
 package dev.yourapp.blem3
-import android.content.*
 
-class BootReceiver: BroadcastReceiver() {
-  override fun onReceive(ctx: Context, intent: Intent) {
-    if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-      ctx.startForegroundService(Intent(ctx, BleM3Service::class.java))
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED && context.autoStart && context.savedAddr != null) {
+            context.startForegroundService(Intent(context, BleM3Service::class.java))
+        }
     }
-  }
 }
