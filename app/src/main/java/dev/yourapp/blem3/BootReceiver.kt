010dc9074739a672e6b02d9d757db16a9e09dc28
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-11-03 02:15:44 +0700

    Update BootReceiver.kt

--- app/src/main/java/dev/yourapp/blem3/BootReceiver.kt
+++ app/src/main/java/dev/yourapp/blem3/BootReceiver.kt
@@ -3,6 +3,8 @@ package dev.yourapp.blem3
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import dev.yourapp.blem3.Prefs.autoStart
+import dev.yourapp.blem3.Prefs.savedAddr
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
